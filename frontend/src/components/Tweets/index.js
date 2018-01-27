22a551d79dbea9ab4da51bb9c9725c6a82f0978a
Katherine Huang <kat.m.huang@gmail.com>
2018-01-27 11:49:36 -0500

    Tweets display state

--- frontend/src/components/Tweets/index.js
+++ frontend/src/components/Tweets/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import Tweet from '../Tweet';
 import './index.css';
 
-const Tweets = (props) => {
-    const tweetList = props.tweets.map((tweet) =>
-        <Tweet key={tweet.id} username={tweet.author} date={tweet.date} content={tweet.content} />
-    )
-	return (
-        <div className="Tweets">
-            {tweetList}
-        </div>
-	);
+class Tweets extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			...props
+		}
+	}
+	render() {
+		const tweetList = this.state.tweets.map((tweet) =>
+        <Tweet key={tweet.id} username={tweet.author} date={tweet.date} content={tweet.content} />)
+
+        return (
+			<div className="Tweets">
+            	{tweetList}
+        	</div>
+        );
+	}
 }
 
 export default Tweets;
